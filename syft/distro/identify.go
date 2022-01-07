b8e7f2d998aac53a1eaf8390c76a25e1d0aacb17
Alex Goodman <alex.goodman@anchore.com>
2022-01-07 17:57:30 -0500

    remove strong distro type
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- syft/distro/identify.go
+++ syft/distro/identify.go
@@ -1,9 +1,14 @@
 package distro
 
 import (
+	"fmt"
 	"io/ioutil"
 	"regexp"
 	"strings"
+
+	"github.com/google/go-cmp/cmp"
+
+	"github.com/acobaugh/osrelease"
 
 	"github.com/anchore/syft/internal"
 
@@ -12,7 +17,7 @@ import (
 )
 
 // returns a distro or nil
-type parseFunc func(string) *Distro
+type parseFunc func(string) (*Release, error)
 
 type parseEntry struct {
 	path string
@@ -31,11 +36,6 @@ var identityFiles = []parseEntry{
 		fn:   parseOsRelease,
 	},
 	{
-		// check for busybox (important to check this last since other distros contain the busybox binary)
-		path: "/bin/busybox",
-		fn:   parseBusyBox,
-	},
-	{
 		// check for centos:6
 		path: "/etc/system-release-cpe",
 		fn:   parseSystemReleaseCPE,
@@ -45,22 +45,22 @@ var identityFiles = []parseEntry{
 		path: "/etc/redhat-release",
 		fn:   parseRedhatRelease,
 	},
+	///////////////////////////////////////////////////////////////////////////////////////////////////////
+	// IMPORTANT! checking busybox must be last since other distros contain the busybox binary
+	{
+		// check for busybox
+		path: "/bin/busybox",
+		fn:   parseBusyBox,
+	},
+	///////////////////////////////////////////////////////////////////////////////////////////////////////
 }
 
 // Identify parses distro-specific files to determine distro metadata like version and release.
-func Identify(resolver source.FileResolver) *Distro {
-	var distro *Distro
-
-identifyLoop:
+func Identify(resolver source.FileResolver) *Release {
 	for _, entry := range identityFiles {
 		locations, err := resolver.FilesByPath(entry.path)
 		if err != nil {
-			log.Errorf("unable to get path locations from %s: %s", entry.path, err)
-			break
-		}
-
-		if len(locations) == 0 {
-			log.Debugf("path not found: %s", entry.path)
+			log.Warnf("unable to get path locations from %s: %+v", entry.path, err)
 			continue
 		}
 
@@ -74,131 +74,119 @@ identifyLoop:
 			content, err := ioutil.ReadAll(contentReader)
 			internal.CloseAndLogError(contentReader, location.VirtualPath)
 			if err != nil {
-				log.Errorf("unable to read %q: %+v", location.RealPath, err)
+				log.Warnf("unable to read %q: %+v", location.RealPath, err)
 				break
 			}
 
-			if len(content) == 0 {
-				log.Debugf("no contents in file, skipping: %s", entry.path)
-				continue
+			release, err := entry.fn(string(content))
+			if err != nil {
+				log.Warnf("unable to parse %q", location.RealPath)
 			}
 
-			if candidateDistro := entry.fn(string(content)); candidateDistro != nil {
-				distro = candidateDistro
-				break identifyLoop
+			if release != nil {
+				return release
 			}
 		}
 	}
 
-	if distro != nil && distro.Type == UnknownDistroType {
-		return nil
-	}
-
-	return distro
+	return nil
 }
 
-func assemble(id, version, like string) *Distro {
-	distroType, ok := IDMapping[id]
-
-	// Both distro and version must be present
-	if len(id) == 0 && len(version) == 0 {
-		return nil
-	}
-
-	// If it's an unknown distro, try mapping the ID_LIKE
-	if !ok && len(like) != 0 {
-		distroType, ok = IDMapping[like]
-	}
-
-	// If we still can't match allow name to be used in constructor
-	if !ok {
-		distroType = Type(id)
-	}
-
-	distro, err := NewDistro(distroType, version, like)
+func parseOsRelease(contents string) (*Release, error) {
+	values, err := osrelease.ReadString(contents)
 	if err != nil {
-		return nil
+		return nil, fmt.Errorf("unable to read os-release file: %w", err)
 	}
 
-	return &distro
-}
-
-func parseOsRelease(contents string) *Distro {
-	id, vers, like := "", "", ""
-	for _, line := range strings.Split(contents, "\n") {
-		parts := strings.Split(line, "=")
-		prefix := parts[0]
-		value := strings.ReplaceAll(parts[len(parts)-1], `"`, "")
-
-		switch prefix {
-		case "ID":
-			id = strings.TrimSpace(value)
-		case "VERSION_ID":
-			vers = strings.TrimSpace(value)
-		case "ID_LIKE":
-			like = strings.TrimSpace(value)
+	var idLike []string
+	for _, s := range strings.Split(values["ID_LIKE"], " ") {
+		s = strings.TrimSpace(s)
+		if s == "" {
+			continue
 		}
+		idLike = append(idLike, s)
 	}
 
-	return assemble(id, vers, like)
+	r := Release{
+		PrettyName:       values["PRETTY_NAME"],
+		Name:             values["NAME"],
+		ID:               values["ID"],
+		IDLike:           idLike,
+		Version:          values["VERSION"],
+		VersionID:        values["VERSION_ID"],
+		Variant:          values["VARIANT"],
+		VariantID:        values["VARIANT_ID"],
+		HomeURL:          values["HOME_URL"],
+		SupportURL:       values["SUPPORT_URL"],
+		BugReportURL:     values["BUG_REPORT_URL"],
+		PrivacyPolicyURL: values["PRIVACY_POLICY_URL"],
+		CPEName:          values["CPE_NAME"],
+	}
+
+	// don't allow for empty contents to result in a Release object being created
+	if cmp.Equal(r, Release{}) {
+		return nil, nil
+	}
+
+	// we always prefer displaying empty collections over nil for slices
+	if r.IDLike == nil {
+		r.IDLike = []string{}
+	}
+
+	return &r, nil
 }
 
 var busyboxVersionMatcher = regexp.MustCompile(`BusyBox v[\d.]+`)
 
-func parseBusyBox(contents string) *Distro {
+func parseBusyBox(contents string) (*Release, error) {
 	matches := busyboxVersionMatcher.FindAllString(contents, -1)
 	for _, match := range matches {
 		parts := strings.Split(match, " ")
 		version := strings.ReplaceAll(parts[1], "v", "")
-		distro := assemble("busybox", version, "")
-		if distro != nil {
-			return distro
-		}
+
+		return simpleRelease(match, "busybox", version, ""), nil
+
 	}
-	return nil
+	return nil, nil
 }
 
-// TODO: we should update parseSystemReleaseCPE to use the CPE struct, pkg.CPE, which requires a refactor to avoid a circular import:
-// TODO: pkg depends on distro to support pURLs. To avoid the circular import, either try to make pkg to not depend on distro (medium lift-ish)
-// TODO: or migrate the cpe code out of the pkg package (small lift).
 // example CPE: cpe:/o:centos:linux:6:GA
 var systemReleaseCpeMatcher = regexp.MustCompile(`cpe:\/o:(.*?):.*?:(.*?):.*?$`)
 
 // parseSystemReleaseCPE parses the older centos (6) file to determine distro metadata
-func parseSystemReleaseCPE(contents string) *Distro {
+func parseSystemReleaseCPE(contents string) (*Release, error) {
 	matches := systemReleaseCpeMatcher.FindAllStringSubmatch(contents, -1)
 	for _, match := range matches {
 		if len(match) < 3 {
-			log.Warnf("system release cpe does not match expected format")
-			return nil
+			return nil, fmt.Errorf("system release cpe does not match expected format")
 		}
-		// note: in SubMatches (capture groups), the 0th index is the full match string
-		// see https://pkg.go.dev/regexp#pkg-overview for more info
-		distro := assemble(match[1], match[2], "")
-		if distro != nil {
-			return distro
-		}
+		return simpleRelease("", match[1], match[2], match[0]), nil
 	}
-	return nil
+	return nil, nil
 }
 
 // example: "CentOS release 6.10 (Final)"
 var redhatReleaseMatcher = regexp.MustCompile(`(.*?)\srelease\s(\d\.\d+)`)
 
 // parseRedhatRelease is a fallback parsing method for determining distro information in older redhat versions
-func parseRedhatRelease(contents string) *Distro {
+func parseRedhatRelease(contents string) (*Release, error) {
 	matches := redhatReleaseMatcher.FindAllStringSubmatch(contents, -1)
 	for _, match := range matches {
 		if len(match) < 3 {
-			log.Warnf("failed to parse redhat-release file, unexpected format")
-			return nil
+			return nil, fmt.Errorf("failed to parse redhat-release file, unexpected format")
 		}
-		// note: in SubMatches (capture groups), the 0th index is the full match string
-		// see https://pkg.go.dev/regexp#pkg-overview for more info
-		distro := assemble(strings.ToLower(match[1]), match[2], "")
-		if distro != nil {
-			return distro
-		}
+		return simpleRelease("", match[1], match[2], ""), nil
 	}
-	return nil
+	return nil, nil
+}
+
+func simpleRelease(prettyName, name, version, cpe string) *Release {
+	return &Release{
+		Name:      name,
+		ID:        name,
+		IDLike:    []string{name},
+		Version:   version,
+		VersionID: version,
+		CPEName:   cpe,
+	}
 }
--- syft/distro/identify_test.go
+++ syft/distro/identify_test.go
@@ -1,357 +1,512 @@
 package distro
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 	"testing"
 
-	"github.com/anchore/syft/internal"
 	"github.com/anchore/syft/syft/source"
-	hashiVer "github.com/hashicorp/go-version"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
-
-const CustomDistro Type = "scientific"
 
 func TestIdentifyDistro(t *testing.T) {
 	tests := []struct {
 		fixture string
-		Type    Type
-		Version string
+		release *Release
 	}{
 		{
 			fixture: "test-fixtures/os/alpine",
-			Type:    Alpine,
-			Version: "3.11.6",
+			release: &Release{
+				PrettyName:   "Alpine Linux v3.11",
+				Name:         "Alpine Linux",
+				ID:           "alpine",
+				IDLike:       []string{},
+				VersionID:    "3.11.6",
+				HomeURL:      "https://alpinelinux.org/",
+				BugReportURL: "https://bugs.alpinelinux.org/",
+			},
 		},
 		{
 			fixture: "test-fixtures/os/amazon",
-			Type:    AmazonLinux,
-			Version: "2.0.0",
+			release: &Release{
+				PrettyName: "Amazon Linux 2",
+				Name:       "Amazon Linux",
+				ID:         "amzn",
+				IDLike: []string{
+					"centos",
+					"rhel",
+					"fedora",
+				},
+				Version:   "2",
+				VersionID: "2",
+				HomeURL:   "https://amazonlinux.com/",
+				CPEName:   "cpe:2.3:o:amazon:amazon_linux:2",
+			},
 		},
 		{
 			fixture: "test-fixtures/os/busybox",
-			Type:    Busybox,
-			Version: "1.31.1",
+			release: &Release{
+				Name:      "busybox",
+				ID:        "busybox",
+				IDLike:    []string{"busybox"},
+				Version:   "1.31.1",
+				VersionID: "1.31.1",
+			},
 		},
 		{
 			fixture: "test-fixtures/os/centos",
-			Type:    CentOS,
-			Version: "8.0.0",
+			release: &Release{
+				PrettyName: "CentOS Linux 8 (Core)",
+				Name:       "CentOS Linux",
+				ID:         "centos",
+				IDLike: []string{"rhel",
+					"fedora",
+				},
+				Version:      "8 (Core)",
+				VersionID:    "8",
+				HomeURL:      "https://www.centos.org/",
+				BugReportURL: "https://bugs.centos.org/",
+				CPEName:      "cpe:/o:centos:centos:8",
+			},
 		},
 		{
 			fixture: "test-fixtures/os/debian",
-			Type:    Debian,
-			Version: "8.0.0",
+			release: &Release{
+				PrettyName:   "Debian GNU/Linux 8 (jessie)",
+				Name:         "Debian GNU/Linux",
+				ID:           "debian",
+				IDLike:       []string{},
+				Version:      "8 (jessie)",
+				VersionID:    "8",
+				HomeURL:      "http://www.debian.org/",
+				SupportURL:   "http://www.debian.org/support",
+				BugReportURL: "https://bugs.debian.org/",
+			},
 		},
 		{
 			fixture: "test-fixtures/os/fedora",
-			Type:    Fedora,
-			Version: "31.0.0",
+			release: &Release{
+				PrettyName:       "Fedora 31 (Container Image)",
+				Name:             "Fedora",
+				ID:               "fedora",
+				IDLike:           []string{},
+				Version:          "31 (Container Image)",
+				VersionID:        "31",
+				Variant:          "Container Image",
+				VariantID:        "container",
+				HomeURL:          "https://fedoraproject.org/",
+				SupportURL:       "https://fedoraproject.org/wiki/Communicating_and_getting_help",
+				BugReportURL:     "https://bugzilla.redhat.com/",
+				PrivacyPolicyURL: "https://fedoraproject.org/wiki/Legal:PrivacyPolicy",
+				CPEName:          "cpe:/o:fedoraproject:fedora:31",
+			},
 		},
 		{
 			fixture: "test-fixtures/os/redhat",
-			Type:    RedHat,
-			Version: "7.3.0",
+			release: &Release{
+				PrettyName:   "Red Hat Enterprise Linux Server 7.3 (Maipo)",
+				Name:         "Red Hat Enterprise Linux Server",
+				ID:           "rhel",
+				IDLike:       []string{"fedora"},
+				Version:      "7.3 (Maipo)",
+				VersionID:    "7.3",
+				HomeURL:      "https://www.redhat.com/",
+				BugReportURL: "https://bugzilla.redhat.com/",
+				CPEName:      "cpe:/o:redhat:enterprise_linux:7.3:GA:server",
+			},
 		},
 		{
 			fixture: "test-fixtures/os/ubuntu",
-			Type:    Ubuntu,
-			Version: "20.4.0",
+			release: &Release{
+				PrettyName:       "Ubuntu 20.04 LTS",
+				Name:             "Ubuntu",
+				ID:               "ubuntu",
+				IDLike:           []string{"debian"},
+				Version:          "20.04 LTS (Focal Fossa)",
+				VersionID:        "20.04",
+				HomeURL:          "https://www.ubuntu.com/",
+				SupportURL:       "https://help.ubuntu.com/",
+				BugReportURL:     "https://bugs.launchpad.net/ubuntu/",
+				PrivacyPolicyURL: "https://www.ubuntu.com/legal/terms-and-policies/privacy-policy",
+			},
 		},
 		{
 			fixture: "test-fixtures/os/oraclelinux",
-			Type:    OracleLinux,
-			Version: "8.3.0",
+			release: &Release{
+				PrettyName:   "Oracle Linux Server 8.3",
+				Name:         "Oracle Linux Server",
+				ID:           "ol",
+				IDLike:       []string{"fedora"},
+				Version:      "8.3",
+				VersionID:    "8.3",
+				Variant:      "Server",
+				VariantID:    "server",
+				HomeURL:      "https://linux.oracle.com/",
+				BugReportURL: "https://bugzilla.oracle.com/",
+				CPEName:      "cpe:/o:oracle:linux:8:3:server",
+			},
 		},
 		{
 			fixture: "test-fixtures/os/empty",
-			Type:    UnknownDistroType,
 		},
 		{
 			fixture: "test-fixtures/os/custom",
-			Type:    CustomDistro,
-			Version: "8.0.0",
+			release: &Release{
+				PrettyName: "CentOS Linux 8 (Core)",
+				Name:       "Scientific Linux",
+				ID:         "scientific",
+				IDLike: []string{
+					"rhel",
+					"fedora",
+				},
+				Version:      "16 (Core)",
+				VersionID:    "8",
+				HomeURL:      "https://www.centos.org/",
+				BugReportURL: "https://bugs.centos.org/",
+				CPEName:      "cpe:/o:centos:centos:8",
+			},
 		},
 		{
 			fixture: "test-fixtures/os/opensuse-leap",
-			Type:    OpenSuseLeap,
-			Version: "15.2.0",
+			release: &Release{
+				PrettyName: "openSUSE Leap 15.2",
+				Name:       "openSUSE Leap",
+				ID:         "opensuse-leap",
+				IDLike: []string{
+					"suse",
+					"opensuse",
+				},
+				Version:      "15.2",
+				VersionID:    "15.2",
+				HomeURL:      "https://www.opensuse.org/",
+				BugReportURL: "https://bugs.opensuse.org",
+				CPEName:      "cpe:/o:opensuse:leap:15.2",
+			},
 		},
 		{
 			fixture: "test-fixtures/os/sles",
-			Type:    SLES,
-			Version: "15.2.0",
+			release: &Release{
+				PrettyName: "SUSE Linux Enterprise Server 15 SP2",
+				Name:       "SLES",
+				ID:         "sles",
+				IDLike:     []string{"suse"},
+				Version:    "15-SP2",
+				VersionID:  "15.2",
+				CPEName:    "cpe:/o:suse:sles:15:sp2",
+			},
 		},
 		{
 			fixture: "test-fixtures/os/photon",
-			Type:    Photon,
-			Version: "2.0.0",
+			release: &Release{
+				PrettyName:   "VMware Photon OS/Linux",
+				Name:         "VMware Photon OS",
+				ID:           "photon",
+				IDLike:       []string{},
+				Version:      "2.0",
+				VersionID:    "2.0",
+				HomeURL:      "https://vmware.github.io/photon/",
+				BugReportURL: "https://github.com/vmware/photon/issues",
+			},
 		},
 		{
 			fixture: "test-fixtures/os/arch",
-			Type:    ArchLinux,
+			release: &Release{
+				PrettyName:   "Arch Linux",
+				Name:         "Arch Linux",
+				ID:           "arch",
+				IDLike:       []string{},
+				HomeURL:      "https://www.archlinux.org/",
+				SupportURL:   "https://bbs.archlinux.org/",
+				BugReportURL: "https://bugs.archlinux.org/",
+			},
 		},
 		{
 			fixture: "test-fixtures/partial-fields/missing-id",
-			Type:    Debian,
-			Version: "8.0.0",
+			release: &Release{
+				Name:      "Debian GNU/Linux",
+				IDLike:    []string{"debian"},
+				VersionID: "8",
+			},
 		},
 		{
 			fixture: "test-fixtures/partial-fields/unknown-id",
-			Type:    Debian,
-			Version: "8.0.0",
+			release: &Release{
+				Name:      "Debian GNU/Linux",
+				ID:        "my-awesome-distro",
+				IDLike:    []string{"debian"},
+				VersionID: "8",
+			},
 		},
 		{
 			fixture: "test-fixtures/partial-fields/missing-version",
-			Type:    UnknownDistroType,
+			release: &Release{
+				Name:   "Debian GNU/Linux",
+				IDLike: []string{"debian"},
+			},
 		},
 		{
 			fixture: "test-fixtures/os/centos6",
-			Type:    CentOS,
-			Version: "6.0.0",
+			release: &Release{
+				Name:      "centos",
+				ID:        "centos",
+				IDLike:    []string{"centos"},
+				Version:   "6",
+				VersionID: "6",
+				CPEName:   "cpe:/o:centos:linux:6:GA",
+			},
 		},
 		{
 			fixture: "test-fixtures/os/centos5",
-			Type:    CentOS,
-			Version: "5.7.0",
+			release: &Release{
+				Name:      "CentOS",
+				ID:        "CentOS",
+				IDLike:    []string{"CentOS"},
+				Version:   "5.7",
+				VersionID: "5.7"},
 		},
 		{
 			fixture: "test-fixtures/os/mariner",
-			Type:    Mariner,
-			Version: "1.0.0",
+			release: &Release{
+				PrettyName:   "CBL-Mariner/Linux",
+				Name:         "Common Base Linux Mariner",
+				ID:           "mariner",
+				IDLike:       []string{},
+				Version:      "1.0.20210901",
+				VersionID:    "1.0",
+				HomeURL:      "https://aka.ms/cbl-mariner",
+				SupportURL:   "https://aka.ms/cbl-mariner",
+				BugReportURL: "https://aka.ms/cbl-mariner",
+			},
 		},
 		{
 			fixture: "test-fixtures/os/rockylinux",
-			Type:    RockyLinux,
-			Version: "8.4.0",
+			release: &Release{
+				PrettyName: "Rocky Linux 8.4 (Green Obsidian)",
+				Name:       "Rocky Linux",
+				ID:         "rocky",
+				IDLike: []string{
+					"rhel",
+					"fedora",
+				},
+				Version:      "8.4 (Green Obsidian)",
+				VersionID:    "8.4",
+				HomeURL:      "https://rockylinux.org/",
+				BugReportURL: "https://bugs.rockylinux.org/",
+				CPEName:      "cpe:/o:rocky:rocky:8.4:GA",
+			},
 		},
 		{
 			fixture: "test-fixtures/os/almalinux",
-			Type:    AlmaLinux,
-			Version: "8.4.0",
+			release: &Release{
+				PrettyName: "AlmaLinux 8.4 (Electric Cheetah)",
+				Name:       "AlmaLinux",
+				ID:         "almalinux",
+				IDLike: []string{
+					"rhel",
+					"centos",
+					"fedora",
+				},
+				Version:      "8.4 (Electric Cheetah)",
+				VersionID:    "8.4",
+				HomeURL:      "https://almalinux.org/",
+				BugReportURL: "https://bugs.almalinux.org/",
+				CPEName:      "cpe:/o:almalinux:almalinux:8.4:GA",
+			},
 		},
 	}
-
-	observedDistros := internal.NewStringSet()
-	definedDistros := internal.NewStringSet()
-
-	for _, distroType := range All {
-		definedDistros.Add(string(distroType))
-	}
-
-	// Somewhat cheating with Windows. There is no support for detecting/parsing a Windows OS, so it is not
-	// possible to comply with this test unless it is added manually to the "observed distros"
-	definedDistros.Remove(string(Windows))
 
 	for _, test := range tests {
 		t.Run(test.fixture, func(t *testing.T) {
 			s, err := source.NewFromDirectory(test.fixture)
-			if err != nil {
-				t.Fatalf("unable to produce a new source for testing: %s", test.fixture)
-			}
+			require.NoError(t, err)
 
 			resolver, err := s.FileResolver(source.SquashedScope)
-			if err != nil {
-				t.Fatalf("unable to get resolver: %+v", err)
-			}
+			require.NoError(t, err)
 
-			d := Identify(resolver)
-			if d == nil {
-				if test.Type == UnknownDistroType {
-					return
-				}
-				t.Fatalf("expected a distro but got none")
-			}
-			observedDistros.Add(d.String())
-
-			if d.Type != test.Type {
-				t.Errorf("expected distro doesn't match: %v != %v", d.Type, test.Type)
-			}
-
-			if d.Type == UnknownDistroType && d.Version != nil {
-				t.Fatalf("version should be nil for unknown distros")
-			} else if d.Type == UnknownDistroType && d.Version == nil {
-				// don't check versions for unknown distro types
-				return
-			}
-
-			if d.Version == nil && test.Version == "" {
-				// this distro does not have a version
-				return
-			}
-
-			assert.Equal(t, d.Version.String(), test.Version)
+			assert.Equal(t, test.release, Identify(resolver))
 		})
-	}
-
-	// ensure that test cases stay in sync with the distros that can be identified
-	if len(observedDistros) < len(definedDistros) {
-		for _, d := range definedDistros.ToSlice() {
-			t.Logf("   defined: %s", d)
-		}
-		for _, d := range observedDistros.ToSlice() {
-			t.Logf("   observed: %s", d)
-		}
-		t.Errorf("distro coverage incomplete (defined=%d, coverage=%d)", len(definedDistros), len(observedDistros))
 	}
 }
 
 func TestParseOsRelease(t *testing.T) {
 
 	tests := []struct {
-		fixture    string
-		name       string
-		RawVersion string
-	}{
-		{
-			fixture:    "test-fixtures/ubuntu-20.04",
-			name:       "ubuntu",
-			RawVersion: "20.04",
-		},
-		{
-			fixture:    "test-fixtures/debian-8",
-			name:       "debian",
-			RawVersion: "8",
-		},
-		{
-			fixture:    "test-fixtures/centos-8",
-			name:       "centos",
-			RawVersion: "8",
-		},
-		{
-			fixture:    "test-fixtures/rhel-8",
-			name:       "redhat",
-			RawVersion: "8.1",
-		},
-		{
-			fixture:    "test-fixtures/unprintable",
-			name:       "debian",
-			RawVersion: "8",
-		},
-	}
-
-	for _, test := range tests {
-		name := fmt.Sprintf("%s:%s", test.name, test.RawVersion)
-		contents := retrieveFixtureContentsAsString(test.fixture, t)
-
-		t.Run(name, func(t *testing.T) {
-			distro := parseOsRelease(contents)
-			if distro.Name() != test.name {
-				t.Errorf("mismatched name in distro: '%s' != '%s'", distro.Name(), test.name)
-			}
-			if distro.RawVersion != test.RawVersion {
-				t.Errorf("mismatched distro version: '%s' != '%s'", distro.RawVersion, test.RawVersion)
-			}
-		})
-	}
-
-}
-
-func TestParseOsReleaseFailures(t *testing.T) {
-
-	tests := []struct {
 		fixture string
-		name    string
+		release *Release
 	}{
 		{
-			fixture: "test-fixtures/bad-id",
-			name:    "No name ID",
+			fixture: "test-fixtures/ubuntu-20.04",
+
+			release: &Release{
+				PrettyName:       "Ubuntu 20.04 LTS",
+				Name:             "Ubuntu",
+				ID:               "ubuntu",
+				IDLike:           []string{"debian"},
+				Version:          "20.04 LTS (Focal Fossa)",
+				VersionID:        "20.04",
+				HomeURL:          "https://www.ubuntu.com/",
+				SupportURL:       "https://help.ubuntu.com/",
+				BugReportURL:     "https://bugs.launchpad.net/ubuntu/",
+				PrivacyPolicyURL: "https://www.ubuntu.com/legal/terms-and-policies/privacy-policy",
+			},
+		},
+
+		{
+			fixture: "test-fixtures/debian-8",
+
+			release: &Release{
+				PrettyName:   "Debian GNU/Linux 8 (jessie)",
+				Name:         "Debian GNU/Linux",
+				ID:           "debian",
+				IDLike:       []string{},
+				Version:      "8 (jessie)",
+				VersionID:    "8",
+				HomeURL:      "http://www.debian.org/",
+				SupportURL:   "http://www.debian.org/support",
+				BugReportURL: "https://bugs.debian.org/",
+			},
+		},
+
+		{
+			fixture: "test-fixtures/centos-8",
+
+			release: &Release{
+				PrettyName: "CentOS Linux 8 (Core)",
+				Name:       "CentOS Linux",
+				ID:         "centos",
+				IDLike: []string{
+					"rhel",
+					"fedora",
+				},
+				Version:      "8 (Core)",
+				VersionID:    "8",
+				HomeURL:      "https://www.centos.org/",
+				BugReportURL: "https://bugs.centos.org/",
+				CPEName:      "cpe:/o:centos:centos:8",
+			},
+		},
+
+		{
+			fixture: "test-fixtures/rhel-8",
+
+			release: &Release{
+				PrettyName:   "Red Hat Enterprise Linux 8.1 (Ootpa)",
+				Name:         "Red Hat Enterprise Linux",
+				ID:           "rhel",
+				IDLike:       []string{"fedora"},
+				Version:      "8.1 (Ootpa)",
+				VersionID:    "8.1",
+				HomeURL:      "https://www.redhat.com/",
+				BugReportURL: "https://bugzilla.redhat.com/",
+				CPEName:      "cpe:/o:redhat:enterprise_linux:8.1:GA",
+			},
+		},
+
+		{
+			fixture: "test-fixtures/unprintable",
+
+			release: &Release{
+				PrettyName:   "Debian GNU/Linux 8 (jessie)",
+				Name:         "Debian GNU/Linux",
+				ID:           "debian",
+				IDLike:       []string{},
+				Version:      "8 (jessie)",
+				VersionID:    "8",
+				HomeURL:      "http://www.debian.org/",
+				SupportURL:   "http://www.debian.org/support",
+				BugReportURL: "https://bugs.debian.org/",
+			},
 		},
 	}
 
 	for _, test := range tests {
-		name := fmt.Sprintf("%s:%s", test.name, test.fixture)
-		contents := retrieveFixtureContentsAsString(test.fixture, t)
-
-		t.Run(name, func(t *testing.T) {
-			distro := parseOsRelease(contents)
-			if distro != nil {
-				t.Errorf("unexpected non-nil distro: '%s' != nil", distro)
-			}
-		})
+		t.Run(test.fixture,
+			func(t *testing.T) {
+				release,
+					err := parseOsRelease(retrieveFixtureContentsAsString(test.fixture,
+					t))
+				require.NoError(t,
+					err)
+				assert.Equal(t,
+					test.release,
+					release)
+			})
 	}
+
 }
 
 func TestParseSystemReleaseCPE(t *testing.T) {
-	centos6Version, _ := hashiVer.NewVersion("6")
 	tests := []struct {
-		fixture  string
-		name     string
-		expected *Distro
+		fixture string
+		release *Release
 	}{
 		{
 			fixture: "test-fixtures/os/centos6/etc/system-release-cpe",
-			name:    "Centos 6",
-			expected: &Distro{
-				Type:       CentOS,
-				Version:    centos6Version,
-				RawVersion: "6",
+			release: &Release{
+				Name:      "centos",
+				ID:        "centos",
+				IDLike:    []string{"centos"},
+				Version:   "6",
+				VersionID: "6",
+				CPEName:   "cpe:/o:centos:linux:6:GA",
 			},
 		},
 		{
-			fixture:  "test-fixtures/bad-system-release-cpe",
-			name:     "Centos 6 Bad CPE",
-			expected: nil,
+			fixture: "test-fixtures/bad-system-release-cpe",
+			release: nil,
 		},
 	}
 
 	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
+		t.Run(test.fixture, func(t *testing.T) {
 			contents := retrieveFixtureContentsAsString(test.fixture, t)
-			actual := parseSystemReleaseCPE(contents)
-
-			if test.expected == nil {
-				assert.Nil(t, actual)
+			release, err := parseSystemReleaseCPE(contents)
+			require.NoError(t, err)
+			if test.release == nil {
+				assert.Nil(t, release)
 				return
 			}
 
-			// not comparing the full distro object because the hashiVer is a pointer
-			assert.Equal(t, test.expected.Type, actual.Type)
-			assert.Equal(t, &test.expected.Version, &actual.Version)
-			assert.Equal(t, test.expected.RawVersion, actual.RawVersion)
+			assert.Equal(t, test.release, release)
 		})
 	}
 }
 
 func TestParseRedhatRelease(t *testing.T) {
-	centos5Version, _ := hashiVer.NewVersion("5.7")
 	tests := []struct {
-		fixture  string
-		name     string
-		expected *Distro
+		fixture string
+		name    string
+		release *Release
 	}{
 		{
 			fixture: "test-fixtures/os/centos5/etc/redhat-release",
 			name:    "Centos 5",
-			expected: &Distro{
-				Type:       CentOS,
-				Version:    centos5Version,
-				RawVersion: "5.7",
+			release: &Release{
+				Name:      "CentOS",
+				ID:        "CentOS",
+				IDLike:    []string{"CentOS"},
+				Version:   "5.7",
+				VersionID: "5.7",
 			},
 		},
 		{
-			fixture:  "test-fixtures/bad-redhat-release",
-			name:     "Centos 5 Bad Redhat Release",
-			expected: nil,
+			fixture: "test-fixtures/bad-redhat-release",
+			name:    "Centos 5 Bad Redhat Release",
+			release: nil,
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			contents := retrieveFixtureContentsAsString(test.fixture, t)
-			actual := parseRedhatRelease(contents)
-
-			if test.expected == nil {
-				assert.Nil(t, actual)
+			release, err := parseRedhatRelease(retrieveFixtureContentsAsString(test.fixture, t))
+			require.NoError(t, err)
+			if test.release == nil {
+				assert.Nil(t, release)
 				return
 			}
 
-			// not comparing the full distro object because the hashiVer is a pointer
-			assert.Equal(t, test.expected.Type, actual.Type)
-			assert.Equal(t, &test.expected.Version, &actual.Version)
-			assert.Equal(t, test.expected.RawVersion, actual.RawVersion)
+			assert.Equal(t, test.release, release)
 		})
 	}
 }
